71c3294eaffb3a478c94925344c871de373f7f7b
johnsonafool <johnsonafool@gmail.com>
2024-01-21 18:50:49 +0800

    feat: update client ci workflow

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+const isTesting = process.env.NODE_ENV === 'test'
+
+function middleware() {
+  return 
+}
+
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+export default isTesting ? authMiddleware({}) : middleware;
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
