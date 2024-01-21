dd643e20f3cfdaf06fd7fa09c53943f295912f5f
johnsonafool <johnsonafool@gmail.com>
2024-01-21 20:28:07 +0800

    fix: docker image process

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,8 +9,7 @@ function middleware() {
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-// export default !isTesting ? authMiddleware({}) : middleware;
-export default middleware;
+export default !isTesting ? authMiddleware({}) : middleware;
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
